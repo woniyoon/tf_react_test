aaab69c39b30489a3bb61a6db9002e6c10060ad4
woniyoon <gellifisch@gmail.com>
2020-11-18 17:17:38 +0900

    Handle text input values changes

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -7,9 +7,28 @@ import CustomButton from "./CustomButton";
 
 const RegisterForm = () => {
     const [memberType, setMemberType] = useState("company");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [companyName, setCompanyName] = useState("");
 
     const handleMemTypeChange = (event) => {
         setMemberType(event.target.value);
+    }
+
+    const handleTextInput = (event) => {
+        const { value, id } = event.target;
+
+        switch (id) {
+            case "email":
+                setEmail(value);
+                break;
+            case "password":
+                setPassword(value);
+                break;
+            case "companyName":
+                setCompanyName(value);
+                break;
+        }
     }
 
     return (
@@ -40,11 +59,31 @@ const RegisterForm = () => {
 
                 <p className="inputsLabel">기본정보 입력</p>
                 <section className="textInputSection">
-                    <TextField className="inputText" label="이메일(아이디)를 입력하세요." variant="outlined" type="email"/>
+                    <TextField 
+                        className="inputText" 
+                        id="email"
+                        label="이메일(아이디)를 입력하세요." 
+                        variant="outlined" 
+                        type="email"
+                        onChange={handleTextInput} 
+                    />
                     <p className="inputDescription">이메일 양식을 확인하세요</p>
-                    <TextField className="inputText" label="비밀번호를 입력하세요." variant="outlined" type="password"/>
+                    <TextField 
+                        className="inputText" 
+                        id="password"
+                        label="비밀번호를 입력하세요." 
+                        variant="outlined" 
+                        type="password"
+                        onChange={handleTextInput} 
+                    />
                     <p className="inputDescription">비밀번호는 8자리 이상, 16자리 이하이고 영문, 숫자, 특수문자가 각 1자리 이상 포함되어야 합니다</p>
-                    <TextField className="inputText" label="회사명을 입력하세요." variant="outlined" />
+                    <TextField 
+                        className="inputText" 
+                        id="companyName"
+                        label="회사명을 입력하세요." 
+                        variant="outlined" 
+                        onChange={handleTextInput}
+                    />
                     <p className="inputDescription">유효한 회사명을 입력해주세요</p>
                 </section>
                 <section className="selectInputSection">
