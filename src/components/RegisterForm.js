990bea6af2466fc13134a8e7cae25039ccfef969
woniyoon <gellifisch@gmail.com>
2020-11-18 15:24:49 +0900

    Add language select input

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -6,62 +6,75 @@ import CustomButton from "./customButton";
 
 const RegisterForm = () => {
     return (
-        <form className="registerForm">
-            <text className="formTitle">Gconstudio 계정 만들기</text>
-            <p className="formDescription">하나의 계정으로 모든 지콘스튜디오 서비스를 이용할 수 있습니다.</p>
-            
-            <FormControl component="fieldset" >
-                <RadioGroup row aria-label="memberType" name="memberType" >
-                    <FormControlLabel 
-                        value="company" 
-                        control={<CustomRadio /> } 
-                        label="번역회사" 
-                    />
-                    <FormControlLabel 
-                        value="client" 
-                        control={<CustomRadio />} 
-                        label="의뢰인"
-                    />
-                    <FormControlLabel 
-                        value="individual" 
-                        control={<CustomRadio />} 
-                        label="번역가" 
-                    />
-                </RadioGroup>
-            </FormControl>
+        <div className="registerPage">
+            <form className="registerForm">
+                <text className="formTitle">Gconstudio 계정 만들기</text>
+                <p className="formDescription">하나의 계정으로 모든 지콘스튜디오 서비스를 이용할 수 있습니다.</p>
+                
+                <FormControl component="fieldset" >
+                    <RadioGroup row aria-label="memberType" name="memberType" >
+                        <FormControlLabel 
+                            value="company" 
+                            control={<CustomRadio /> } 
+                            label="번역회사" 
+                        />
+                        <FormControlLabel 
+                            value="client" 
+                            control={<CustomRadio />} 
+                            label="의뢰인"
+                        />
+                        <FormControlLabel 
+                            value="individual" 
+                            control={<CustomRadio />} 
+                            label="번역가" 
+                        />
+                    </RadioGroup>
+                </FormControl>
 
-            <p>기본정보 입력</p>
-            <section className="textInputSection">
-                <TextField className="inputText" label="이메일(아이디)를 입력하세요." variant="outlined" />
-                <TextField className="inputText" label="비밀번호를 입력하세요." variant="outlined" />
-                <TextField className="inputText" label="회사명을 입력하세요." variant="outlined" />
-            </section>
-            <section className="selectInputSection">
-                <Select
-                    className="inputSelect"
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                >
-                    <MenuItem disabled>
-                        <p>화폐단위를 선택하세요.</p>
-                    </MenuItem>
-                    <MenuItem value="krw">Korean Won</MenuItem>
-                    <MenuItem value="usd">US Dollar</MenuItem>
-                </Select>
-                <Select
-                    className="inputSelect"
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                >
-                    <MenuItem disabled>
-                        <p>설립일을 선택하세요.</p>
-                    </MenuItem>
-                    <MenuItem value="krw">Korean Won</MenuItem>
-                    <MenuItem value="usd">US Dollar</MenuItem>
-                </Select>
-            </section>
-            <CustomButton className="nextBtn">다음</CustomButton>
-        </form>
+                <p>기본정보 입력</p>
+                <section className="textInputSection">
+                    <TextField className="inputText" label="이메일(아이디)를 입력하세요." variant="outlined" />
+                    <TextField className="inputText" label="비밀번호를 입력하세요." variant="outlined" />
+                    <TextField className="inputText" label="회사명을 입력하세요." variant="outlined" />
+                </section>
+                <section className="selectInputSection">
+                    <Select
+                        className="inputSelect"
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                        <MenuItem disabled>
+                            <p>화폐단위를 선택하세요.</p>
+                        </MenuItem>
+                        <MenuItem value="krw">Korean Won</MenuItem>
+                        <MenuItem value="usd">US Dollar</MenuItem>
+                    </Select>
+                    <Select
+                        className="inputSelect"
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                        <MenuItem disabled>
+                            <p>설립일을 선택하세요.</p>
+                        </MenuItem>
+                        <MenuItem value="krw">Korean Won</MenuItem>
+                        <MenuItem value="usd">US Dollar</MenuItem>
+                    </Select>
+                </section>
+                <section>
+                    <CustomButton className="nextBtn">다음</CustomButton>
+                    <a className="loginLink">로그인</a>
+                </section>
+            </form>
+            <Select
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                defaultValue="kor"
+            >
+                <MenuItem value="kor">한국어</MenuItem>
+                <MenuItem value="eng">영어</MenuItem>
+            </Select>
+        </div>
     );
 
 }
