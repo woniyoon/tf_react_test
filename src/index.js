49b36c021d45435bca935ee96085d93cc13b1797
woniyoon <gellifisch@gmail.com>
2020-11-20 19:20:16 +0900

    Make double indentation

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import RegisterForm from "./components/RegisterForm";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <RegisterForm />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RegisterForm />
+    </React.StrictMode>,
   document.getElementById("root")
 );
